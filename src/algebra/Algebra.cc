0a2daffcaa2017dfc8523ec5b7c5864fdac6a854
FakeEmperor <fake_empire@mail.ru>
2017-04-15 15:06:12 +0300

    Changed removed normalize() method

--- src/algebra/Algebra.cc
+++ src/algebra/Algebra.cc
@@ -1,6 +1,6 @@
 #include "Algebra.h"
 
-explicit size_t Algebra::mod(int a, size_t Zp)
+size_t algebra::mod(int a, size_t Zp)
 {
 	if (a < 0)
 		return Zp - (-a) % Zp;
@@ -8,25 +8,16 @@ explicit size_t Algebra::mod(int a, size_t Zp)
 		return a % Zp;
 }
 
-size_t Algebra::mod(size_t a, size_t Zp) {
+size_t algebra::mod(size_t a, size_t Zp) {
     return a % Zp;
 }
 
-
-std::vector<size_t> Algebra::normalize(const std::vector<int>& vec, size_t Zp)
-{
-	std::vector<size_t> res(vec.size());
-	for (size_t i = 0, s = vec.size(); i<s; ++i)
-		res[i] = mod(vec[i], Zp);
-	return res;
-}
-
-size_t Algebra::randmod(std::function<int()> generator, size_t Zp)
+size_t algebra::randmod(std::function<int()> generator, size_t Zp)
 {
 	return mod(generator(), Zp);
 }
 
-size_t Algebra::powmod(int a, size_t pow, size_t Zp)
+size_t algebra::powmod(int a, size_t pow, size_t Zp)
 {
 	size_t res = 1;
 	while (pow--)
@@ -37,7 +28,7 @@ size_t Algebra::powmod(int a, size_t pow, size_t Zp)
 	return res;
 }
 
-size_t Algebra::gcd(size_t a, size_t b)
+size_t algebra::gcd(size_t a, size_t b)
 {
 	if (a < b)
 		std::swap(a, b);
@@ -58,7 +49,7 @@ size_t Algebra::gcd(size_t a, size_t b)
 	return a;
 }
 
-size_t Algebra::expanded_gcd(size_t a, size_t b, size_t Zp)
+size_t algebra::expanded_gcd(size_t a, size_t b, size_t Zp)
 {
 	a = mod(a, Zp);
 	b = mod(b, Zp);
@@ -93,7 +84,7 @@ size_t Algebra::expanded_gcd(size_t a, size_t b, size_t Zp)
 	return mod(matrix[1][3] * b, Zp);
 }
 
-size_t Algebra::inverse_mod(size_t a, size_t Zp)
+size_t algebra::inverse_mod(size_t a, size_t Zp)
 {
 	return expanded_gcd(a, 1, Zp);
 }
--- src/algebra/Algebra.h
+++ src/algebra/Algebra.h
@@ -3,12 +3,19 @@
 #include <vector>
 #include <functional>
 
-namespace Algebra
+namespace algebra
 {
-	size_t explicit mod(int a, size_t Zp);
+	size_t mod(int a, size_t Zp);
 	size_t mod(size_t a, size_t Zp);
 
-	std::vector<size_t> normalize(const std::vector<int>& vec, size_t Zp);
+	template <typename IntegralType>
+    std::vector<size_t> normalize(const std::vector<IntegralType>& vec, size_t Zp)
+    {
+        std::vector<size_t> res(vec.size());
+        for (size_t i = 0, s = vec.size(); i<s; ++i)
+            res[i] = mod(vec[i], Zp);
+        return res;
+    }
 
 	size_t randmod(std::function<int()> generator, size_t Zp);
 
