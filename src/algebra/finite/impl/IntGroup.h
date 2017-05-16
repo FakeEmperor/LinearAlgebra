338376532baa0ef739a9bfdf9eb323c2f81adb8c
FakeEmperor <fake_empire@mail.ru>
2017-05-17 02:51:58 +0300

    IntGroup.h: Code fixes in correlation with Group.h more stubs for future methods CRTP fixes

--- src/algebra/finite/impl/IntGroup.h
+++ src/algebra/finite/impl/IntGroup.h
@@ -6,6 +6,9 @@
 #define SRC_ALGEBRA_FINITE_IMPL_INTGROUP_H
 
 #include <memory>
+#include <ostream>
+#include <set>
+#include <utils/utils.h>
 #include "../Group.h"
 
 namespace algebra::finite::impl {
@@ -16,10 +19,17 @@ namespace algebra::finite::impl {
     class IntAdditiveGroup;
 
 
-    template <typename IntegerType, typename = std::enable_if<std::is_integral<IntegerType>::value>::type>
-    class IntAdditiveFGroupElement: public FGroupElement<IntegerType, IntegerType(0), IntAdditiveFGroupElement<IntegerType>> {
+    template <typename IntegralType>
+    class IntAdditiveFGroupElement: public FGroupElement<IntegralType, IntegralType(0), IntAdditiveFGroupElement> {
     public:
-        typedef FGroupElement<IntegerType, IntegerType(0), IntAdditiveFGroupElement<IntegerType>> Base;
+        static_assert(std::is_integral<IntegralType>::value,
+                      "IntAdditiveFGroupElement can be defined for only integral types");
+
+		#ifndef _MSC_VER
+        typedef FGroupElement<IntegralType, IntegralType(0), IntAdditiveFGroupElement> Base;
+		#else 
+		typedef FGroupElement Base;
+    	#endif
         // Yes, typedefs are not "inherited" - you need to full name for them
         // cause this: https://isocpp.org/wiki/faq/templates#nondependent-name-lookup-members
         // So this is to "simulate" desired behaviour
@@ -28,15 +38,14 @@ namespace algebra::finite::impl {
         typedef typename Base::Group Group;
 
         IntAdditiveFGroupElement(const IntAdditiveFGroupElement &) = default;
-        IntAdditiveFGroupElement(const IntegerType &value, const Group *bound_group) : value_(value), bound_group_(bound_group) {};
+        IntAdditiveFGroupElement(const IntegralType &value, const Group *bound_group) : value_(value), bound_group_(bound_group) {};
 
         ~IntAdditiveFGroupElement() override final = default;
 
-        operator IntegerType() const override {
-            return value_;
-        }
-
-        Ptr operator+(const IntAdditiveFGroupElement &a) const override {
+//        explicit operator IntegralType() const override {
+//            return value_;
+//        }
+        Ptr operator+(const IntAdditiveFGroupElement<IntegralType> &a) const override {
             return bound_group_->operateInside(this->value_ + a.value_);
         }
 
@@ -44,8 +53,8 @@ namespace algebra::finite::impl {
             return bound_group_->inverse(*this);
         }
 
-        bool operator<(const IntAdditiveFGroupElement &a) const override {
-            return value_ < a;
+        bool operator<(const IntAdditiveFGroupElement<IntegralType> &a) const override {
+            return value_ < a.value_;
         }
 
         std::unique_ptr<std::set<Ptr>> stabilizator() const override {
@@ -68,51 +77,46 @@ namespace algebra::finite::impl {
             throw ::utils::NotImplementedError();
         }
 
-        IntegerType operator*() const override {
+        IntegralType operator*() const override {
             return value_;
         }
 
-        std::ofstream &print(std::ofstream &ofstream, const Group &group) const override {
-            return ofstream << value_;
+
+        std::ostream &print(std::ostream &ostream, const Group &group) const override {
+            throw ::utils::NotImplementedError();
         }
 
-        operator IntegralType() const override {
-            return nullptr;
-        }
 
     protected:
-        IntegerType value_;
+        IntegralType value_;
         GroupPtr bound_group_; // non-owning reference to the group. Be careful with it
 
 
     };
 
 
-    template <typename IntegerType>
-    class IntAdditiveGroup : public Group<IntAdditiveFGroupElement<IntegerType>> {
+    template <typename IntegralType>
+    class IntAdditiveGroup : public Group<IntegralType, IntAdditiveFGroupElement> {
     public:
-        typedef Group<IntAdditiveFGroupElement<IntegerType>> Base;
+        typedef Group<IntegralType, IntAdditiveFGroupElement> Base;
         typedef typename Base::ElemType ElemType;
-        typedef typename Base::IntegralType IntegralType;
-
 
         IntAdditiveGroup(const IntAdditiveGroup& ) = default;
-        IntAdditiveGroup(const IntAdditiveGroup&&) = default;
-        IntAdditiveGroup(const IntegerType &modulus) : modulus_(modulus) {}
+        IntAdditiveGroup(IntAdditiveGroup&&) noexcept = default;
+        explicit IntAdditiveGroup(const IntegralType &modulus) : modulus_(modulus) {}
 
         ~IntAdditiveGroup() override = default;
-
-        typename ElemType::Ptr operateInside(const IntegralType &a) const override {
-            return ElemType::make(a % modulus_);
+        typename Base::ElemType::Ptr operateInside(const IntegralType &a) const override {
+            return ElemType::make(a % modulus_, this);
         }
 
-        typename ElemType::Ptr inverse(const ElemType &type) const override {
-            return ElemType::make(modulus_ - *type );
+        typename ElemType::Ptr inverse(const ElemType &e) const override {
+            return ElemType::make(modulus_ - *e, this);
         }
 
         typename ElemType::Ptr operator[](size_t index) const override {
-            // this is kind of a hack - we know, that index of the element
-            // is the element itself
+            // this is kind of a hack - we know that index of the element
+            // is the element itself modulo group's order
             return operateInside(IntegralType(index));
         }
 
@@ -124,17 +128,19 @@ namespace algebra::finite::impl {
             throw ::utils::NotImplementedError();
         }
 
-        std::ofstream& print(std::ofstream &ofstream, const Base &group) const override {
+        std::ostream& print(std::ostream &ofstream, const Base &group) const override {
             throw ::utils::NotImplementedError();
+        }
+
+        void pregenerate() const {
+            // do nothing - it's cheaper to generate
         }
 
         FFGroupProperties properties() const override {
             return FFGroupProperties::FGP_INVALID;
         }
-
     protected:
-        const IntegerType& modulus_;
-
+        const IntegralType& modulus_;
     };
 
 
